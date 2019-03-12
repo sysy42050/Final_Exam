ff56aa59d7a9c5ce32d01c3cc62f58c880a381ab
Syann Hollins <02syann.hollins@gulllakecs.org>
2019-03-12 09:56:22 -0400

    Imported necessary modules

--- For Loop.py
+++ For Loop.py
@@ -1,10 +1,8 @@
 # Syann Hollins
-#3/7/19
-# Calendar
+# 3/7/19
+# Dice Roll
 
-for i in range(1,13):
-   print (i)
-enter_a_num_month = int(input("Enter a number for the month: "))
+# Imported necessary modules
+import random
 
-print("")
 
