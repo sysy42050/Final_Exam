aa2ade9ac9f99ef868c6067f5165442fbb5f60f0
Syann Hollins <02syann.hollins@gulllakecs.org>
2019-03-14 07:59:34 -0400

    Added Function

--- For Loop.py
+++ For Loop.py
@@ -6,22 +6,24 @@
 
 import random
 
-how_many_times = input("How many times do you want to roll the dice: ")
+how_many_times = int(input("How many times do you want to roll the dice: "))
 
 min = 1
 max = 6
 
- for i in range(how_many_times):
+
+for i in range(how_many_times):
     roll_again = random.randint(1,6)
 
-
-while True:
-    guess = int(input("Enter value 1 to 6 : "))
-    if guess == roll_again:
-        print("You got it!")
-        break
-    print("Try again")
-    print("")
+def user_guess(roll_again):
+    while True:
+        guess = int(input("Enter value 1 to 6 : "))
+        if guess == roll_again:
+            print("You got it!")
+            break
+        print("Try again")
+        print("")
+user_guess(roll_again)
 
 total = 0
 
